c48e252c178540c3f0833862306b53f8d176cc46
Vince Gonzalez <vgonzalez@mapr.com>
2014-11-15 23:55:49 +0000

    one shot build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,19 +5,13 @@
 
 test -d $EC2_HOME || exit -1
 
-$EC2_HOME/bin/ec2-delete-keypair $EC2_KEYPAIR
-$EC2_HOME/bin/ec2-create-keypair $EC2_KEYPAIR | grep -A 50 BEGIN > $PEM_FILE
-chmod 0600 $PEM_FILE
-
 ansible-playbook \
-	--extra-vars "ec2_keypair=$EC2_KEYPAIR ec2_name_tag=$CLUSTER_NAME" \
 	-i hosts \
-	--private-key="$PEM_FILE" "$WD/playbooks/aws_bootstrap.yml"
+	"$WD/playbooks/aws_bootstrap.yml"
 
-ansible all -i playbooks/cluster.hosts --private-key $PEM_FILE -m ping
+ansible all -i playbooks/cluster.hosts -m ping
 
 ansible-playbook \
-	--extra-vars "ec2_keypair=$EC2_KEYPAIR ec2_name_tag=$CLUSTER_NAME" \
 	-i playbooks/cluster.hosts \
-	--private-key="$PEM_FILE" "$WD/playbooks/install_cluster.yml"
+	"$WD/playbooks/install_cluster.yml"
 
